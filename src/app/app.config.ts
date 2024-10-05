5f465dfbea2675de27bdb0f31a2dba4759ee2e4f
Gustavo <gustavonrezende@gmail.com>
2024-10-05 21:48:17 +0000

     x

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
-// src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+import { importProvidersFrom } from '@angular/core';
 import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './auth/auth.service';  // Corrigindo o caminho do AuthService
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideAuth(() => getAuth())
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(FormsModule),
+    AuthService  // O AuthService será registrado automaticamente por estar em 'root'
   ]
 };
