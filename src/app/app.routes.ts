cab206d222ccb1b8136e88694555af1c688096d0
Gustavo <gustavonrezende@gmail.com>
2024-10-05 20:35:29 +0000

     post login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AppComponent } from './app.component';
+import { PostLoginComponent } from './post-login/post-login.component';  // Novo componente
 
 export const routes: Routes = [
-   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' } // Rota padrão redireciona para login
+  { path: 'login', component: LoginComponent },
+  { path: 'post-login', component: PostLoginComponent },  // Rota pós-login
+  { path: '', redirectTo: '/login', pathMatch: 'full' },  // Redireciona para o login por padrão
 ];
-
-
---
+++ src/app/post-login/post-login.component.ts
@@ -0,0 +1,10 @@
+// src/app/post-login/post-login.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-login',
+  templateUrl: './post-login.component.html',
+  styleUrls: ['./post-login.component.css'],
+  standalone: true
+})
+export class PostLoginComponent {}
